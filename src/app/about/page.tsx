f2706c3fc1cede7aa9e8906d5941df14f9998bd4
Elisha Anagnostakis <elishastaks@gmail.com>
2024-01-30 21:36:37 +1100

    refactor: added metadata generation for all pages which should improve seo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,14 @@ import AboutUs from '@/components/AboutUs';
 import CallToAction from '@/components/CallToAction';
 import Mission from '@/components/Mission';
 
+export async function generateMetadata() {
+  return {
+    title: 'About Us - The Eggsperts',
+    description:
+      'Discover the story behind The Eggsperts - your go-to source for premium eggs and honey. Dive into our commitment to quality, sustainable practices, and the pure goodness that sets us apart. Join us in celebrating the essence of nature, one egg and jar of honey at a time.',
+  };
+}
+
 const page = () => {
   return (
     <>
--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,6 +1,14 @@
 import TopBar from '@/components/TopBar';
 import { BaseTemplate } from '@/templates/BaseTemplate';
 
+export async function generateMetadata() {
+  return {
+    title: 'The Eggsperts - Explore Our Range of Quality Products',
+    description:
+      'Discover a diverse range of premium products at The Eggsperts. From farm-fresh eggs to pure honey, explore our selection and find detailed information on each product. Choose quality, choose freshness with The Eggsperts.',
+  };
+}
+
 export default function Layout({
   children, // will be a page or nested layout
 }: {
